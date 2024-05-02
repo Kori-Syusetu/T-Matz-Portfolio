84612fb202b903f35ce8eb8604b26094da7066f2
Kori Syusetu <kori@isnow.jp>
2024-05-02 10:55:55 +0900

    Initial Commit

---
+++ components/bsIcon.vue
@@ -0,0 +1,30 @@
+<template>
+	<i :class="'bi bi-' + iconName + additionalClass"></i>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	class?: string | string[],
+	icon?: string,
+}>();
+
+const additionalClass = toRef("");
+const iconName = toRef("");
+
+if (props.class) {
+	if (typeof props.class == "string") {
+		additionalClass.value = ` ${props.class}`;
+	} else {
+		additionalClass.value = "";
+		for (const addClass of props.class) {
+			additionalClass.value += ` ${addClass}`;
+		}
+	}
+}
+
+if (props.icon) {
+	iconName.value = props.icon;
+} else {
+	iconName.value = "";
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+	<header>
+		<!-- Insert Header Menu -->
+	</header>
+	<main>
+		<slot/>
+	</main>
+	<footer>
+		<!-- Insert Footer Navigation -->
+	</footer>
+</template>
+
+<script setup lang="ts">
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "assets/plex.less";
+</script>
