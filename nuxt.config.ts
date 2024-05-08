a48ba9554d3b577a045426d3c901287ec71ef2e6
崎津 祥汰 <shota.sakitsu@virtlive.jp>
2024-05-08 14:02:21 +0900

    PWAほか諸々

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,58 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			meta: [
+				{name: "theme-color", content: "#FFAFAF"},
+			],
+			link: [
+				{rel: 'icon', href: `/favicon.ico`, sizes: "48x48"},
+				{rel: 'apple-touch-icon', href: `/apple-touch-icon-180x180.png`},
+			],
+		},
+	},
+	modules: [
+		'@vite-pwa/nuxt',
+	],
+	pwa: {
+		registerType: "autoUpdate",
+		manifest: {
+			name: 'T Matz Portfolio',
+			description: "ポートフォリオページです。",
+			theme_color: "#FFAFAF",
+			lang: "ja",
+			short_name: "T.M. Portfolio",
+			start_url: "/",
+			display: "standalone",
+			background_color: "#ffffff",
+			icons: [
+				{
+					"src": "pwa-64x64.png",
+					"sizes": "64x64",
+					"type": "image/png"
+				},
+				{
+					"src": "pwa-192x192.png",
+					"sizes": "192x192",
+					"type": "image/png"
+				},
+				{
+					"src": "pwa-512x512.png",
+					"sizes": "512x512",
+					"type": "image/png"
+				},
+				{
+					"src": "maskable-icon-512x512.png",
+					"sizes": "512x512",
+					"type": "image/png",
+					"purpose": "maskable"
+				},
+			],
+		},
+		workbox: {
+			navigateFallback: null,
+		},
+	},
 	devtools: {
 		enabled: false,
 	},
@@ -14,13 +67,4 @@ export default defineNuxtConfig({
 		]
 	},
 	ssr: true,
-	app: {
-		head: {
-			script: [
-				{
-					src: "https://cdn.jsdelivr.net/npm/vue3-google-map"
-				}
-			]
-		}
-	}
 })
--- pages/links.vue
+++ pages/links.vue
@@ -1,38 +1,43 @@
 <template>
 	<article>
+		<h1>Links</h1>
+		<p>
+			私関連のリンク集です。
+		</p>
 		<section>
-			<h1>Links</h1>
-			<p>
-				私関連のリンク集です。
-			</p>
+			<h2>Social Networks</h2>
 			<section>
-				<h2>Social Networks</h2>
+				<h3>Non-Fediverse</h3>
+				<v-list>
+					<v-list-item link href="https://x.com/Maria_Kamikawa" title="X (Hobby)" subtitle="@Maria_Kamikawa"/>
+					<v-list-item link href="https://x.com/Hinata_Hojo" title="X (Technology)" subtitle="@Hinata_Hojo"/>
+					<v-list-item link href="https://x.com/Shota_Sakitsu" title="X (Virtual Talent)" subtitle="@Shota_Sakitsu"/>
+					<v-list-item link href="https://www.instagram.com/kori_syusetu.isnow/" title="Instagram (Main)" subtitle="@kori_syusetu.isnow"/>
+					<v-list-item link href="https://www.instagram.com/shota.sakitsu/" title="Instagram (Virtual Talent)" subtitle="@shota.sakitsu"/>
+					<v-list-item link href="https://www.youtube.com/@Kori16Syusetu" title="YouTube (Main)" subtitle="@Kori16Syusetu"/>
+					<v-list-item link href="https://www.youtube.com/@Shota-Sakitsu" title="YouTube (Virtual Talent)" subtitle="@Shota-Sakitsu"/>
+				</v-list>
+			</section>
+			<section>
+				<h3>Fediverse</h3>
 				<section>
-					<h3>Non-Fediverse</h3>
+					<h4>ActivityPub</h4>
 					<v-list>
-						<v-list-item link href="https://x.com/Maria_Kamikawa" title="X (Hobby)" subtitle="@Maria_Kamikawa"/>
-						<v-list-item link href="https://x.com/Hinata_Hojo" title="X (Technology)" subtitle="@Hinata_Hojo"/>
-						<v-list-item link href="https://x.com/Shota_Sakitsu" title="X (Virtual Talent)" subtitle="@Shota_Sakitsu"/>
-						<v-list-item link href="https://www.instagram.com/kori_syusetu.isnow/" title="Instagram (Main)" subtitle="@kori_syusetu.isnow"/>
-						<v-list-item link href="https://www.instagram.com/shota.sakitsu/" title="Instagram (Virtual Talent)" subtitle="@shota.sakitsu"/>
-						<v-list-item link href="https://www.youtube.com/@Kori16Syusetu" title="YouTube (Main)" subtitle="@Kori16Syusetu"/>
-						<v-list-item link href="https://www.youtube.com/@Shota-Sakitsu" title="YouTube (Virtual Talent)" subtitle="@Shota-Sakitsu"/>
+						<v-list-item link href="https://misskey.afy.jp/@Kori16Syusetu" title="IcyMisskey" subtitle="@Kori16Syusetu@misskey.afy.jp"/>
+						<v-list-item link href="https://misskey.io/@Kori16Syusetu" title="Misskey.io" subtitle="@Kori16Syusetu@Misskey.io"/>
+						<v-list-item link href="https://vtdon.com/@shota_sakitsu" title="VTdon (Virtual Talent)" subtitle="@shota_sakitsu@vtdon.com"/>
+						<v-list-item link href="https://www.threads.net/@kori_syusetu.isnow" title="Threads (Main)" subtitle="@kori_syusetu.isnow@threads.net"/>
+						<v-list-item link href="https://www.threads.net/@shota.sakitsu" title="Threads (Virtual Talent)" subtitle="@shota.sakitsu@threads.net"/>
 					</v-list>
 				</section>
-				<section>
-					<h3>Fediverse</h3>
-					<section>
-						<h4>ActivityPub</h4>
-						<v-list>
-							<v-list-item link href="https://misskey.afy.jp/@Kori16Syusetu" title="IcyMisskey" subtitle="@Kori16Syusetu@misskey.afy.jp"/>
-							<v-list-item link href="https://misskey.io/@Kori16Syusetu" title="Misskey.io" subtitle="@Kori16Syusetu@Misskey.io"/>
-							<v-list-item link href="https://vtdon.com/@shota_sakitsu" title="VTdon (Virtual Talent)" subtitle="@shota_sakitsu@vtdon.com"/>
-							<v-list-item link href="https://www.threads.net/@kori_syusetu.isnow" title="Threads (Main)" subtitle="@kori_syusetu.isnow@threads.net"/>
-							<v-list-item link href="https://www.threads.net/@shota.sakitsu" title="Threads (Virtual Talent)" subtitle="@shota.sakitsu@threads.net"/>
-						</v-list>
-					</section>
-				</section>
 			</section>
+		</section>
+		<section>
+			<h2>Source Code</h2>
+			<v-list>
+				<v-list-item link href="https://git.isnow.jp/kori-syusetu/t-matz-portfolio" title="IcyGitLab"/>
+				<v-list-item link href="https://github.com/Kori-Syusetu/T-Matz-Portfolio" title="GitHub (Mirror)"/>
+			</v-list>
 		</section>
 	</article>
 </template>
